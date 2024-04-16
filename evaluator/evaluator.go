8364a73a5adb0c1d6382845db94c6d6fce3688b2
Stanislau Tur <stanislau.tur@gmail.com>
2024-04-16 13:41:46 +0200

    eval unary expressions

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -26,6 +26,8 @@ func Eval(node ast.Node) object.Object {
 			return TRUE
 		}
 		return FALSE
+	case *ast.UnaryExpression:
+		return evalUnaryExpression(node.Operator, Eval(node.Right))
 	}
 
 	return nil
@@ -39,4 +41,35 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	}
 
 	return result
+}
+
+func evalUnaryExpression(op string, right object.Object) object.Object {
+	switch op {
+	case "!":
+		return evalBangOperator(right)
+	case "-":
+		return evalMinusUnaryExpression(right)
+	default:
+		return nil
+	}
+}
+
+func evalBangOperator(obj object.Object) object.Object {
+	switch obj {
+	case TRUE:
+		return FALSE
+	case FALSE, NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
+}
+
+func evalMinusUnaryExpression(obj object.Object) object.Object {
+	integer, ok := obj.(*object.Integer)
+	if !ok {
+		return NULL
+	}
+
+	return &object.Integer{Value: -integer.Value}
 }
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -15,6 +15,8 @@ func TestEvalIntegerExpression(t *testing.T) {
 	}{
 		{"5", 5},
 		{"10", 10},
+		{"-5", -5},
+		{"-10", -10},
 	}
 
 	for _, tt := range tests {
@@ -76,4 +78,23 @@ func testBooleanObject(t *testing.T, o object.Object, expected bool) bool {
 	}
 
 	return true
+}
+
+func TestBangOpearator(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"!true", false},
+		{"!false", true},
+		{"!5", false},
+		{"!!true", true},
+		{"!!false", false},
+		{"!!5", true},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
 }
